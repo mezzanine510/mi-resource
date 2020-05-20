4586219205c69a3afc82ca2113a5e27c4c9a1148
Mezz <mezzanine510@gmail.com>
2020-05-20 00:08:25 -0700

    removed some comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,3 @@
-//Search API
-
-// This API accepts POST requests and requires a HTTP Basic authorization header. The API accepts a JSON object in the request body as input. The “full text” search filter is named “q”. The field level search filters generally take the form of [property name]_[operator], where operator is “Equals”, “Matches”, “Exists”, “NotExists”, “Before”, “After”, “GreaterThan”, or “LessThan”. Maximum result set size is 2000 (search object field name “max”). You can use paging to grab more results by copying the “searchAfter” field from the previous response to the subsequent request body.
-
-// POST = https://qcz87jizmc.execute-api.us-east-1.amazonaws.com/dev/tm/promo/search-products
-
 const url = 'https://qcz87jizmc.execute-api.us-east-1.amazonaws.com/dev/tm/promo/search-products';
 const data = {};
 const loginForm = document.getElementById('loginForm');
